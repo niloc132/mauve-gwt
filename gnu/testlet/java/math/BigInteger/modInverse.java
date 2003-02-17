fde731f4937d50be48a922b5517fb9190432365d
raif <raif>
2003-02-17 19:48:54 +0000

    + added a test for recursive invocation of modInverse().

--- gnu/testlet/java/math/BigInteger/modInverse.java
+++ gnu/testlet/java/math/BigInteger/modInverse.java
@@ -1,4 +1,4 @@
-// $Id: modInverse.java,v 1.1 2002/12/15 15:20:10 mark Exp $
+// $Id: modInverse.java,v 1.2 2003/02/17 19:48:54 raif Exp $
 //
 // Copyright (C) 2002 Free Software Foundation, Inc.
 //
@@ -30,7 +30,7 @@ import gnu.testlet.TestHarness;
 /**
  * Test of <code>modeInv()</code> and subordinate methods.
  *
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class modInverse implements Testlet
 {
@@ -59,8 +59,28 @@ public class modInverse implements Testlet
 
   public void test(TestHarness harness)
   {
+    testRecursion(harness);
     for (int i = 0; i < ps.length; i++)
       testCase(harness, i, ps[i], qs[i]);
+  }
+
+  private void testRecursion(TestHarness harness)
+  {
+    harness.checkPoint("testRecursion");
+    try
+    {
+      BigInteger k = new BigInteger("1a1eb1e6b8f115eee3dc1334afc7de2f7efbd568", 16);
+      BigInteger q = new BigInteger("de09f1902cf484f232fee5d27262372d1c6072d7", 16);
+      BigInteger t = new BigInteger("a3a790f0b7d2bea3a81dc676032cf99c23c28bee", 16);
+      BigInteger tt = k.modInverse(q);
+//      harness.check(true, "k.modInverse(q)="+tt.toString(16));
+      harness.check(t.equals(k.modInverse(q)), "recursion ok");
+    }
+    catch (Exception x)
+    {
+      harness.debug(x);
+      harness.fail(String.valueOf(x));
+    }
   }
 
   private void testCase(TestHarness harness, int i, String ps, String qs)
